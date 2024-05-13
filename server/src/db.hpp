50b07af1117a7ec60d6ea112a44bec9e126c05f4
artemka-sh <artemmmchik@mail.ru>
2024-05-13 20:35:01 +0300

    ыщьу

--- server/src/db.hpp
+++ server/src/db.hpp
@@ -1,31 +1,29 @@
 #pragma once
-#ifndef DB_MANAGER
-#define DB_MANAGER
+
 #include<iostream>
 #include<pqxx/pqxx>
 
 
-bool test(){
+void test(){
     try
     {
         pqxx::connection *c;
-	c = new pqxx::connection("host=217.197.240.93 user=nyashka password=cmd dbname = chat port=5432");
-	std::cout << "[SUCCESS] DB CONNECTION" << std::endl;
-	pqxx::work w(*c);
-	pqxx::result result = w.exec("SELECT * FROM chats");
-	w.commit();
-	for(auto res : result){
-		for(auto r : res){
-			std::cout << r << " ";
+		c = new pqxx::connection("host=217.197.240.93 user=nyashka password=cmd dbname = chat port=5432");
+		std::cout << "[SUCCESS] DB CONNECTION" << std::endl;
+		pqxx::work w(*c);
+		pqxx::result result = w.exec("SELECT * FROM chats");
+		w.commit();
+		for(auto res : result){
+			for(auto r : res){
+				std::cout << r << " ";
+			}
+			std::cout << std::endl;
 		}
-		std::cout << std::endl;
-	}
     }
     catch (std::exception const &e)
     {
         std::cerr << e.what() << std::endl;
-        return 1;
     }
 }
 
-#endif
+
