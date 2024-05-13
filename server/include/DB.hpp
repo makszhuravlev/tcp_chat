4ebf7dd773589707c3a005afe67746a75e3da64d
artemka-sh <artemmmchik@mail.ru>
2024-05-13 21:48:43 +0300

    finish

--- server/include/DB.hpp
+++ server/include/DB.hpp
@@ -9,31 +9,9 @@ private:
 	pqxx::connection *c;
 public:
 	DB(); 
+	void test();
 };  
 
 
-void test();
-/*
-{
-    try
-    {
-        pqxx::connection *c;
-		c = new pqxx::connection("host=217.197.240.93 user=nyashka password=cmd dbname = chat port=5432");
-		std::cout << "[SUCCESS] DB CONNECTION" << std::endl;
-		pqxx::work w(*c);
-		pqxx::result result = w.exec("SELECT * FROM chats");
-		w.commit();
-		for(auto res : result){
-			for(auto r : res){
-				std::cout << r << " ";
-			}
-			std::cout << std::endl;
-		}
-    }
-    catch (std::exception const &e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-}
-*/
+
 
--- server/src/DB.cpp
+++ server/src/DB.cpp
@@ -3,9 +3,29 @@
 DB::DB()
 {
 	c = new pqxx::connection("host=217.197.240.93 user=nyashka password=cmd dbname = chat port=5432");
+	std::cout << "[SUCCESS] DB CONNECTION" << std::endl;
 }
 
 
-void test(){
+void DB::test(){
 	std::cout << "test" << std::endl;
+	{
+		try
+		{
+			pqxx::work w(*c);
+			pqxx::result result = w.exec("SELECT * FROM chats");
+			w.commit();
+			for(auto res : result){
+				for(auto r : res){
+					std::cout << r << " ";
+				}
+				std::cout << std::endl;
+			}
+		}
+		catch (std::exception const &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+
 }
