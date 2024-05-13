9a3a0009df9454de6a5a2d29e80bd210cce8ff9f
artemka-sh <artemmmchik@mail.ru>
2024-05-13 19:21:03 +0300

    SOME

---
+++ build/CMakeFiles/server.dir/main.cpp.o.d
@@ -0,0 +1,250 @@
+CMakeFiles/server.dir/main.cpp.o: \
+ /home/Artem/vscode/tcp_chat/server/main.cpp /usr/include/stdc-predef.h \
+ /usr/include/c++/11.4.0/iostream \
+ /usr/include/c++/11.4.0/x86_64-rosa-linux-gnu/bits/c++config.h \
+ /usr/include/c++/11.4.0/x86_64-rosa-linux-gnu/bits/os_defines.h \
+ /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/c++/11.4.0/x86_64-rosa-linux-gnu/bits/cpu_defines.h \
+ /usr/include/c++/11.4.0/pstl/pstl_config.h \
+ /usr/include/c++/11.4.0/ostream /usr/include/c++/11.4.0/ios \
+ /usr/include/c++/11.4.0/iosfwd /usr/include/c++/11.4.0/bits/stringfwd.h \
+ /usr/include/c++/11.4.0/bits/memoryfwd.h \
+ /usr/include/c++/11.4.0/bits/postypes.h /usr/include/c++/11.4.0/cwchar \
+ /usr/include/wchar.h /usr/include/bits/libc-header-start.h \
+ /usr/include/bits/floatn.h /usr/include/bits/floatn-common.h \
+ /usr/lib64/gcc/x86_64-rosa-linux-gnu/11.4.0/include/stddef.h \
+ /usr/lib64/gcc/x86_64-rosa-linux-gnu/11.4.0/include/stdarg.h \
+ /usr/include/bits/wchar.h /usr/include/bits/types/wint_t.h \
+ /usr/include/bits/types/mbstate_t.h \
+ /usr/include/bits/types/__mbstate_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h /usr/include/bits/types/locale_t.h \
+ /usr/include/bits/types/__locale_t.h /usr/include/c++/11.4.0/exception \
+ /usr/include/c++/11.4.0/bits/exception.h \
+ /usr/include/c++/11.4.0/bits/exception_ptr.h \
+ /usr/include/c++/11.4.0/bits/exception_defines.h \
+ /usr/include/c++/11.4.0/bits/cxxabi_init_exception.h \
+ /usr/include/c++/11.4.0/typeinfo \
+ /usr/include/c++/11.4.0/bits/hash_bytes.h /usr/include/c++/11.4.0/new \
+ /usr/include/c++/11.4.0/bits/move.h /usr/include/c++/11.4.0/type_traits \
+ /usr/include/c++/11.4.0/bits/nested_exception.h \
+ /usr/include/c++/11.4.0/bits/char_traits.h \
+ /usr/include/c++/11.4.0/bits/stl_algobase.h \
+ /usr/include/c++/11.4.0/bits/functexcept.h \
+ /usr/include/c++/11.4.0/bits/cpp_type_traits.h \
+ /usr/include/c++/11.4.0/ext/type_traits.h \
+ /usr/include/c++/11.4.0/ext/numeric_traits.h \
+ /usr/include/c++/11.4.0/bits/stl_pair.h \
+ /usr/include/c++/11.4.0/bits/stl_iterator_base_types.h \
+ /usr/include/c++/11.4.0/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/11.4.0/bits/concept_check.h \
+ /usr/include/c++/11.4.0/debug/assertions.h \
+ /usr/include/c++/11.4.0/bits/stl_iterator.h \
+ /usr/include/c++/11.4.0/bits/ptr_traits.h \
+ /usr/include/c++/11.4.0/debug/debug.h \
+ /usr/include/c++/11.4.0/bits/predefined_ops.h \
+ /usr/include/c++/11.4.0/cstdint \
+ /usr/lib64/gcc/x86_64-rosa-linux-gnu/11.4.0/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/types.h \
+ /usr/include/bits/timesize.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/time64.h /usr/include/bits/stdint-intn.h \
+ /usr/include/bits/stdint-uintn.h \
+ /usr/include/c++/11.4.0/bits/localefwd.h \
+ /usr/include/c++/11.4.0/x86_64-rosa-linux-gnu/bits/c++locale.h \
+ /usr/include/c++/11.4.0/clocale /usr/include/locale.h \
+ /usr/include/bits/locale.h /usr/include/c++/11.4.0/cctype \
+ /usr/include/ctype.h /usr/include/bits/endian.h \
+ /usr/include/bits/endianness.h /usr/include/c++/11.4.0/bits/ios_base.h \
+ /usr/include/c++/11.4.0/ext/atomicity.h \
+ /usr/include/c++/11.4.0/x86_64-rosa-linux-gnu/bits/gthr.h \
+ /usr/include/c++/11.4.0/x86_64-rosa-linux-gnu/bits/gthr-default.h \
+ /usr/include/pthread.h /usr/include/sched.h \
+ /usr/include/bits/types/time_t.h \
+ /usr/include/bits/types/struct_timespec.h /usr/include/bits/sched.h \
+ /usr/include/bits/types/struct_sched_param.h /usr/include/bits/cpu-set.h \
+ /usr/include/time.h /usr/include/bits/time.h /usr/include/bits/timex.h \
+ /usr/include/bits/types/struct_timeval.h \
+ /usr/include/bits/types/clock_t.h /usr/include/bits/types/struct_tm.h \
+ /usr/include/bits/types/clockid_t.h /usr/include/bits/types/timer_t.h \
+ /usr/include/bits/types/struct_itimerspec.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/thread-shared-types.h \
+ /usr/include/bits/pthreadtypes-arch.h /usr/include/bits/struct_mutex.h \
+ /usr/include/bits/struct_rwlock.h /usr/include/bits/setjmp.h \
+ /usr/include/bits/types/__sigset_t.h \
+ /usr/include/bits/types/struct___jmp_buf_tag.h \
+ /usr/include/c++/11.4.0/x86_64-rosa-linux-gnu/bits/atomic_word.h \
+ /usr/include/sys/single_threaded.h \
+ /usr/include/c++/11.4.0/bits/locale_classes.h \
+ /usr/include/c++/11.4.0/string /usr/include/c++/11.4.0/bits/allocator.h \
+ /usr/include/c++/11.4.0/x86_64-rosa-linux-gnu/bits/c++allocator.h \
+ /usr/include/c++/11.4.0/ext/new_allocator.h \
+ /usr/include/c++/11.4.0/bits/ostream_insert.h \
+ /usr/include/c++/11.4.0/bits/cxxabi_forced.h \
+ /usr/include/c++/11.4.0/bits/stl_function.h \
+ /usr/include/c++/11.4.0/backward/binders.h \
+ /usr/include/c++/11.4.0/bits/range_access.h \
+ /usr/include/c++/11.4.0/initializer_list \
+ /usr/include/c++/11.4.0/bits/basic_string.h \
+ /usr/include/c++/11.4.0/ext/alloc_traits.h \
+ /usr/include/c++/11.4.0/bits/alloc_traits.h \
+ /usr/include/c++/11.4.0/bits/stl_construct.h \
+ /usr/include/c++/11.4.0/string_view \
+ /usr/include/c++/11.4.0/bits/functional_hash.h \
+ /usr/include/c++/11.4.0/bits/string_view.tcc \
+ /usr/include/c++/11.4.0/ext/string_conversions.h \
+ /usr/include/c++/11.4.0/cstdlib /usr/include/stdlib.h \
+ /usr/include/bits/waitflags.h /usr/include/bits/waitstatus.h \
+ /usr/include/sys/types.h /usr/include/endian.h \
+ /usr/include/bits/byteswap.h /usr/include/bits/uintn-identity.h \
+ /usr/include/sys/select.h /usr/include/bits/select.h \
+ /usr/include/bits/types/sigset_t.h /usr/include/alloca.h \
+ /usr/include/bits/stdlib-float.h /usr/include/c++/11.4.0/bits/std_abs.h \
+ /usr/include/c++/11.4.0/cstdio /usr/include/stdio.h \
+ /usr/include/bits/types/__fpos_t.h /usr/include/bits/types/__fpos64_t.h \
+ /usr/include/bits/types/struct_FILE.h \
+ /usr/include/bits/types/cookie_io_functions_t.h \
+ /usr/include/bits/stdio_lim.h /usr/include/c++/11.4.0/cerrno \
+ /usr/include/errno.h /usr/include/bits/errno.h \
+ /usr/include/linux/errno.h /usr/include/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/include/bits/types/error_t.h \
+ /usr/include/c++/11.4.0/bits/charconv.h \
+ /usr/include/c++/11.4.0/bits/basic_string.tcc \
+ /usr/include/c++/11.4.0/bits/locale_classes.tcc \
+ /usr/include/c++/11.4.0/system_error \
+ /usr/include/c++/11.4.0/x86_64-rosa-linux-gnu/bits/error_constants.h \
+ /usr/include/c++/11.4.0/stdexcept /usr/include/c++/11.4.0/streambuf \
+ /usr/include/c++/11.4.0/bits/streambuf.tcc \
+ /usr/include/c++/11.4.0/bits/basic_ios.h \
+ /usr/include/c++/11.4.0/bits/locale_facets.h \
+ /usr/include/c++/11.4.0/cwctype /usr/include/wctype.h \
+ /usr/include/bits/wctype-wchar.h \
+ /usr/include/c++/11.4.0/x86_64-rosa-linux-gnu/bits/ctype_base.h \
+ /usr/include/c++/11.4.0/bits/streambuf_iterator.h \
+ /usr/include/c++/11.4.0/x86_64-rosa-linux-gnu/bits/ctype_inline.h \
+ /usr/include/c++/11.4.0/bits/locale_facets.tcc \
+ /usr/include/c++/11.4.0/bits/basic_ios.tcc \
+ /usr/include/c++/11.4.0/bits/ostream.tcc /usr/include/c++/11.4.0/istream \
+ /usr/include/c++/11.4.0/bits/istream.tcc /usr/local/include/pqxx/pqxx \
+ /usr/local/include/pqxx/internal/header-pre.hxx \
+ /usr/include/c++/11.4.0/version \
+ /usr/local/include/pqxx/config-public-compiler.h \
+ /usr/include/c++/11.4.0/ciso646 /usr/local/include/pqxx/array.hxx \
+ /usr/include/c++/11.4.0/algorithm /usr/include/c++/11.4.0/utility \
+ /usr/include/c++/11.4.0/bits/stl_relops.h \
+ /usr/include/c++/11.4.0/bits/stl_algo.h \
+ /usr/include/c++/11.4.0/bits/algorithmfwd.h \
+ /usr/include/c++/11.4.0/bits/stl_heap.h \
+ /usr/include/c++/11.4.0/bits/stl_tempbuf.h \
+ /usr/include/c++/11.4.0/bits/uniform_int_dist.h \
+ /usr/include/c++/11.4.0/pstl/glue_algorithm_defs.h \
+ /usr/include/c++/11.4.0/functional /usr/include/c++/11.4.0/tuple \
+ /usr/include/c++/11.4.0/array \
+ /usr/include/c++/11.4.0/bits/uses_allocator.h \
+ /usr/include/c++/11.4.0/bits/invoke.h \
+ /usr/include/c++/11.4.0/bits/refwrap.h \
+ /usr/include/c++/11.4.0/bits/std_function.h \
+ /usr/include/c++/11.4.0/unordered_map \
+ /usr/include/c++/11.4.0/ext/aligned_buffer.h \
+ /usr/include/c++/11.4.0/bits/hashtable.h \
+ /usr/include/c++/11.4.0/bits/hashtable_policy.h \
+ /usr/include/c++/11.4.0/bits/enable_special_members.h \
+ /usr/include/c++/11.4.0/bits/node_handle.h \
+ /usr/include/c++/11.4.0/bits/unordered_map.h \
+ /usr/include/c++/11.4.0/bits/erase_if.h /usr/include/c++/11.4.0/vector \
+ /usr/include/c++/11.4.0/bits/stl_uninitialized.h \
+ /usr/include/c++/11.4.0/bits/stl_vector.h \
+ /usr/include/c++/11.4.0/bits/stl_bvector.h \
+ /usr/include/c++/11.4.0/bits/vector.tcc \
+ /usr/include/c++/11.4.0/pstl/execution_defs.h \
+ /usr/include/c++/11.4.0/cassert /usr/include/assert.h \
+ /usr/local/include/pqxx/connection.hxx /usr/include/c++/11.4.0/cstddef \
+ /usr/include/c++/11.4.0/ctime /usr/include/c++/11.4.0/list \
+ /usr/include/c++/11.4.0/bits/stl_list.h \
+ /usr/include/c++/11.4.0/bits/allocated_ptr.h \
+ /usr/include/c++/11.4.0/bits/list.tcc /usr/include/c++/11.4.0/map \
+ /usr/include/c++/11.4.0/bits/stl_tree.h \
+ /usr/include/c++/11.4.0/bits/stl_map.h \
+ /usr/include/c++/11.4.0/bits/stl_multimap.h \
+ /usr/include/c++/11.4.0/memory \
+ /usr/include/c++/11.4.0/bits/stl_raw_storage_iter.h \
+ /usr/include/c++/11.4.0/bits/align.h /usr/include/c++/11.4.0/bit \
+ /usr/include/c++/11.4.0/bits/unique_ptr.h \
+ /usr/include/c++/11.4.0/bits/shared_ptr.h \
+ /usr/include/c++/11.4.0/bits/shared_ptr_base.h \
+ /usr/include/c++/11.4.0/ext/concurrence.h \
+ /usr/include/c++/11.4.0/bits/shared_ptr_atomic.h \
+ /usr/include/c++/11.4.0/bits/atomic_base.h \
+ /usr/include/c++/11.4.0/bits/atomic_lockfree_defines.h \
+ /usr/include/c++/11.4.0/backward/auto_ptr.h \
+ /usr/include/c++/11.4.0/pstl/glue_memory_defs.h \
+ /usr/local/include/pqxx/errorhandler.hxx \
+ /usr/local/include/pqxx/types.hxx /usr/include/c++/11.4.0/iterator \
+ /usr/include/c++/11.4.0/bits/stream_iterator.h \
+ /usr/local/include/pqxx/except.hxx \
+ /usr/local/include/pqxx/internal/concat.hxx \
+ /usr/local/include/pqxx/strconv.hxx /usr/include/c++/11.4.0/charconv \
+ /usr/include/c++/11.4.0/cstring /usr/include/string.h \
+ /usr/include/strings.h /usr/include/c++/11.4.0/limits \
+ /usr/include/c++/11.4.0/sstream /usr/include/c++/11.4.0/bits/sstream.tcc \
+ /usr/include/c++/11.4.0/ranges /usr/local/include/pqxx/util.hxx \
+ /usr/local/include/pqxx/version.hxx \
+ /usr/local/include/pqxx/internal/libpq-forward.hxx \
+ /usr/local/include/pqxx/internal/ignore-deprecated-pre.hxx \
+ /usr/local/include/pqxx/internal/ignore-deprecated-post.hxx \
+ /usr/local/include/pqxx/zview.hxx \
+ /usr/local/include/pqxx/internal/conversions.hxx \
+ /usr/include/c++/11.4.0/numeric \
+ /usr/include/c++/11.4.0/bits/stl_numeric.h \
+ /usr/include/c++/11.4.0/pstl/glue_numeric_defs.h \
+ /usr/include/c++/11.4.0/optional /usr/include/c++/11.4.0/variant \
+ /usr/include/c++/11.4.0/bits/parse_numbers.h \
+ /usr/local/include/pqxx/params.hxx \
+ /usr/local/include/pqxx/internal/statement_parameters.hxx \
+ /usr/local/include/pqxx/binarystring.hxx \
+ /usr/local/include/pqxx/result.hxx \
+ /usr/local/include/pqxx/internal/encodings.hxx \
+ /usr/include/c++/11.4.0/iomanip /usr/include/c++/11.4.0/locale \
+ /usr/include/c++/11.4.0/bits/locale_facets_nonio.h \
+ /usr/include/c++/11.4.0/x86_64-rosa-linux-gnu/bits/time_members.h \
+ /usr/include/c++/11.4.0/x86_64-rosa-linux-gnu/bits/messages_members.h \
+ /usr/include/libintl.h /usr/include/c++/11.4.0/bits/codecvt.h \
+ /usr/include/c++/11.4.0/bits/locale_facets_nonio.tcc \
+ /usr/include/c++/11.4.0/bits/locale_conv.h \
+ /usr/include/c++/11.4.0/bits/quoted_string.h \
+ /usr/local/include/pqxx/internal/encoding_group.hxx \
+ /usr/local/include/pqxx/separated_list.hxx \
+ /usr/local/include/pqxx/internal/array-composite.hxx \
+ /usr/local/include/pqxx/blob.hxx /usr/include/c++/11.4.0/filesystem \
+ /usr/include/c++/11.4.0/bits/fs_fwd.h /usr/include/c++/11.4.0/chrono \
+ /usr/include/c++/11.4.0/ratio /usr/include/c++/11.4.0/bits/fs_path.h \
+ /usr/include/c++/11.4.0/codecvt /usr/include/c++/11.4.0/bits/fs_dir.h \
+ /usr/include/c++/11.4.0/bits/fs_ops.h /usr/include/c++/11.4.0/span \
+ /usr/local/include/pqxx/dbtransaction.hxx \
+ /usr/local/include/pqxx/transaction_base.hxx \
+ /usr/local/include/pqxx/internal/stream_query.hxx \
+ /usr/local/include/pqxx/internal/gates/connection-stream_from.hxx \
+ /usr/local/include/pqxx/internal/callgate.hxx \
+ /usr/local/include/pqxx/internal/stream_iterator.hxx \
+ /usr/local/include/pqxx/transaction_focus.hxx \
+ /usr/local/include/pqxx/isolation.hxx /usr/local/include/pqxx/row.hxx \
+ /usr/local/include/pqxx/field.hxx /usr/local/include/pqxx/composite.hxx \
+ /usr/local/include/pqxx/internal/stream_query_impl.hxx \
+ /usr/local/include/pqxx/cursor.hxx \
+ /usr/local/include/pqxx/internal/sql_cursor.hxx \
+ /usr/local/include/pqxx/largeobject.hxx \
+ /usr/local/include/pqxx/nontransaction.hxx \
+ /usr/local/include/pqxx/transaction.hxx \
+ /usr/local/include/pqxx/notification.hxx \
+ /usr/local/include/pqxx/pipeline.hxx \
+ /usr/local/include/pqxx/prepared_statement.hxx \
+ /usr/local/include/pqxx/params.hxx /usr/local/include/pqxx/range.hxx \
+ /usr/local/include/pqxx/internal/result_iterator.hxx \
+ /usr/local/include/pqxx/internal/result_iter.hxx \
+ /usr/local/include/pqxx/robusttransaction.hxx \
+ /usr/local/include/pqxx/stream_from.hxx \
+ /usr/local/include/pqxx/stream_to.hxx \
+ /usr/local/include/pqxx/subtransaction.hxx \
+ /usr/local/include/pqxx/time.hxx /usr/local/include/pqxx/transactor.hxx \
+ /usr/local/include/pqxx/internal/header-post.hxx
