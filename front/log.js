31624385bfbd6bdef9fcb471896c98b0d7b359c3
makszhuravlev <makszhuravlev2006@gmail.com>
2024-05-15 19:04:34 +0300

    createjsonrequests

---
+++ front/log.js
@@ -0,0 +1,28 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+    // формирование json
+    const data = {
+        type: 'log',
+        username: username,
+        password: password
+    };
+    // отправка
+    fetch('URL_TO_LOGIN_API', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Request-Type': 'login'  
+        },
+        body: JSON.stringify(data)
+    })
+    // проверка
+    .then(response => response.json())
+    .then(data => {
+        console.log('Успех:', data);
+    })
+    .catch((error) => {
+        console.error('Ошибка:', error);
+    });
+});
--- front/reg.js
+++ front/reg.js
@@ -1,5 +1,7 @@
 document.getElementById('registerForm').addEventListener('submit', function(event) {
     event.preventDefault();
+    const username = document.getElementById('registerUsername').value;
+    const email = document.getElementById('email').value;
     const password = document.getElementById('registerPassword').value;
     const confirmPassword = document.getElementById('confirmPassword').value;
     const errorMessage = document.getElementById('error');
@@ -8,8 +10,28 @@ document.getElementById('registerForm').addEventListener('submit', function(even
         errorMessage.textContent = 'Пароли не совпадают';
     } else {
         errorMessage.textContent = '';
-        // Продолжить регистрацию
-        console.log('Регистрация прошла успешно');
-        // Здесь можно добавить код для отправки данных формы на сервер
+        // формирование json
+        const data = {
+            type:'reg',
+            username: username,
+            email: email,
+            password: password
+        };
+        console.log('Отправка данных регистрации:', JSON.stringify(data));
+        // отправка
+        fetch('URL_TO_REGISTER_API', {
+            method: 'POST',
+            headers: {
+            },
+            body: JSON.stringify(data)
+        })
+        // проверка 
+        .then(response => response.json())
+        .then(data => {
+            console.log('Успех:', data);
+        })
+        .catch((error) => {
+            console.error('Ошибка:', error);
+        });
     }
 });
