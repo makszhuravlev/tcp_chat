5d24b45f2a48704391b8c23b9603038329bd61d3
artemka-sh <artemmmchik@mail.ru>
2024-05-16 15:38:52 +0300

    🐳

--- server/include/DBManager.hpp
+++ server/include/DBManager.hpp
@@ -10,31 +10,45 @@
 
 using json = nlohmann::json;
 
-struct command{
-    int userID;
-    std::string action;
-};
-
 class DBManager
 {
 private:
-    pqxx::connection *c; 
-    void connect();    
-    void disconnect();
+    pqxx::connection *c;
+    void connect(){
+      try{
+	std::string json_data = parseJSON_to_string("serverinfo.json"); 
+	json serverJSON = json::parse(json_data);
+	std::string connection_info = "host="+serverJSON["ipAddress"].get<std::string>()+" port="+std::to_string((int)serverJSON["port"])+" user="+serverJSON["username"].get<std::string>()+" password="+serverJSON["password"].get<std::string>()+" dbname="+serverJSON["database"].get<std::string>();
+        c = new pqxx::connection(connection_info);
+        std::cout << "[SUCCESS] DBManager CONNECTED" << std::endl;
+      }
+      catch(std::exception const &e){
+        std::cerr << e.what() << std::endl;
+      }
+    }    
 public:
-    DBManager(); 
-    ~DBManager();
+    DBManager()
+    {
+	connect();
+    }
+    ~DBManager(){
+      std::cout << c << std::endl;
+	if(c != nullptr){
+		c->close();
+	}
+    }
+    
+    std::string parseJSON_to_string(const std::string& file_name){
+	std::ifstream file(file_name);
+        if (!file.is_open()) {
+            throw std::runtime_error("Failed to open file");
+        }
 
+	std::stringstream buffer;
+        buffer << file.rdbuf();
+        std::string jsonData = buffer.str();
 
-
-    pqxx::connection* getConnection() {return c;}
-
-    std::string selectFromChat(command* cmd);
-    void registrationRequest(std::string NAVERNO_JSON);
-
-    //tools
-    std::vector<std::string> splitMessage(std::string s, std::string delimeter = ";");
-    std::string messageManager(std::string message);
-    std::string parseJSON_to_string(const std::string& file_name);
+	return jsonData;
+    }
 };
 
