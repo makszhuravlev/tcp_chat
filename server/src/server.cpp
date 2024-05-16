dce508c686f09fffc15b099ffb98454e846dbe0e
artemka-sh <artemmmchik@mail.ru>
2024-05-16 15:51:28 +0300

    🐳

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -17,33 +17,33 @@ int main() {
         tcp::acceptor acceptor{ioc, {address, port}};
         
         while(true){
-          tcp::socket socket{ioc};
-          acceptor.accept(socket);
-          std::cout << "socket connected" << std::endl;
-          DBManager* ClientDB = new DBManager();
-          std::thread{
-            [socket{std::move(socket)}](){
-              boost::beast::websocket::stream<tcp::socket> ws {std::move(const_cast<tcp::socket&>(socket))};
-              
-              ws.accept();
-              
-              while(1){
-                try{
-                  boost::beast::flat_buffer buffer;
-                  ws.read(buffer);
-                  auto message = boost::beast::buffers_to_string(buffer.cdata());
-                  std::cout << message << std::endl;
-                  ws.write(buffer.data());
-                }
-                catch(boost::beast::system_error const& se){
-                  if(se.code() != boost::beast::websocket::error::closed){
-                    std::cout << se.code().message() << std::endl;
-                    break;
-                  }
-                }
-              }
-              
-            } 
+			tcp::socket socket{ioc};
+			acceptor.accept(socket);
+			std::cout << "socket connected" << std::endl;
+			DBManager* ClientDB = new DBManager();
+			std::thread{
+				[socket{std::move(socket)}](){
+				boost::beast::websocket::stream<tcp::socket> ws {std::move(const_cast<tcp::socket&>(socket))};
+				
+				ws.accept();
+				
+				while(true){
+					try{
+					boost::beast::flat_buffer buffer;
+					ws.read(buffer);
+					auto message = boost::beast::buffers_to_string(buffer.cdata());
+					std::cout << message << std::endl;
+					ws.write(buffer.data());
+					}
+					catch(boost::beast::system_error const& se){
+					if(se.code() != boost::beast::websocket::error::closed){
+						std::cout << se.code().message() << std::endl;
+						break;
+					}
+					}
+				}
+				
+				} 
           }.detach();
           
         }
