5ad9889e97a048c9d2e33cc7bbd6e08e1db113bf
Alexandr <s.stepantzover@yandex.ru>
2024-05-23 14:08:19 +0300

    STABLE+-

--- front/script.js
+++ front/script.js
@@ -1,29 +1,13 @@
 let username = sessionStorage.getItem('username');
 let password = sessionStorage.getItem('password');
-let CURRCHID = null;
+let CURRCHID = 0;
+let CHATS = [];
 console.log(username, password);
 console.log(username, password);
 let smski = null;
-
+var socket = new WebSocket('ws://217.197.240.93:8080');
 
 function startNonBlockingLoop() {
-    // Use setInterval to call a function every 1000 milliseconds (1 second)
-    const intervalId = setInterval(() => {
-        console.log('This runs every second without blocking the UI.');
-        loadChatMessages()
-        // Perform your operations here
-    }, 1000);
-
-    return intervalId;
-}
-
-// const loopId = startNonBlockingLoop();
-
-
-
-let currentChatId = null;
-var socket = new WebSocket('ws://217.197.240.93:8080');
-        socket.addEventListener('open', function (event) {
         console.log('Connected to WS Server');
         
         var jsonData = {
@@ -43,58 +27,79 @@ var socket = new WebSocket('ws://217.197.240.93:8080');
             console.error('WebSocket error: ', event);
             document.getElementById("error").textContent="Ошибка соединения с сервером";
         });
-    });
 
-    
+    // Use setInterval to call a function every 1000 milliseconds (1 second)
+    const intervalId = setInterval(() => {
+        console.log('This runs every second without blocking the UI.');
+        loadChatMessages()
+        // Perform your operations here
+    }, 1000);
+
+    return intervalId;
+}
+
+
+
+
+socket.addEventListener('open', function (event) {
+    const loopId = startNonBlockingLoop();
+    });    
 function loadChatList() {
 
     const chatListElement = document.getElementById('chatList');
     chatListElement.innerHTML = '';
+    
     socket.addEventListener('message', function (event) {
-        console.log('Message from server:', event.data);
+        
+        // console.log('Message from server:-----', event.data);
         const response = JSON.parse(event.data);
-        chats = response;     
-        chats.forEach(chat => {
-            const chatItem = document.createElement('div');
-            chatItem.classList.add('chat-item');
-            chatItem.setAttribute('data-chat-id', chat.chat_id);
-    
-            const chatAvatar = document.createElement('div');
-            chatAvatar.classList.add('chat-avatar');
-    
-            const chatInfo = document.createElement('div');
-            chatInfo.classList.add('chat-info');
-    
-            const chatName = document.createElement('div');
-            chatName.classList.add('chat-name');
-            chatName.textContent = chat.name;
-    
-            chatInfo.appendChild(chatName);
-            chatItem.appendChild(chatAvatar);
-            chatItem.appendChild(chatInfo);
-    
-            chatItem.addEventListener('click', () => {
-                currentChatId = chat.id;
-                loadChatMessages();
-                CURRCHID = chat.chat_id;
-                console.log(chat.chat_id);
+        console.log("sss", response);
+        if(response != true && response != null){
+            chats = response;     
+            chats.forEach(chat => {
                 
+                console.log("Check", CHATS);
+                if( !(CHATS.includes(chat.chat_id))){
+                    const chatItem = document.createElement('div');
+                    chatItem.classList.add('chat-item');
+                    chatItem.setAttribute('data-chat-id', chat.chat_id);
+                    CHATS.push(chat.chat_id);
+                    const chatAvatar = document.createElement('div');
+                    chatAvatar.classList.add('chat-avatar');
+            
+                    const chatInfo = document.createElement('div');
+                    chatInfo.classList.add('chat-info');
+                    const chatName = document.createElement('div');
+                    chatName.classList.add('chat-name');
+                    chatName.textContent = chat.name;
+            
+                    chatInfo.appendChild(chatName);
+                    chatItem.appendChild(chatAvatar);
+                    chatItem.appendChild(chatInfo);
+                    
+                    chatItem.addEventListener('click', () => {
+                        
+                            currentChatId = chat.id;
+                            loadChatMessages();
+                            CURRCHID = chat.chat_id;
+                            console.log("Change" , chat.chat_id);
+                        
+                        
+                    });
+            
+                    chatListElement.appendChild(chatItem);
+                }
                 
-
-            });
-    
-            chatListElement.appendChild(chatItem);
-        });
+            }); }
+        
     });
 
 
     
 }
 function loadChatMessages() {
-    
-    var socket = new WebSocket('ws://217.197.240.93:8080');
-    socket.addEventListener('open', function (event) {
-    console.log('Connected to WS Server');
+    if (CURRCHID != 0){
+
     
     var jsonData = {
         type: 1,
@@ -108,16 +113,16 @@ function loadChatMessages() {
     
     console.log("Sending:", jsonString);
     socket.send(jsonString);
-});
     socket.addEventListener('message', function (event) {
         const messagesElement = document.getElementById('messages');
         messagesElement.innerHTML = '';
-        smski = JSON.parse(event.data)
+        console.log("NotJson", event.data);
+
+        if(event.data != 'true' && event.data != 'null'){
         JSON.parse(event.data).forEach(element => {
             const messageContainer = document.createElement('div');
             messageContainer.textContent = element["content"];
             if (element["author_id"] === username) {
-                console.log(11)
                 messageContainer.classList.add('message', 'message-sent');
             } else{
                 messageContainer.classList.add('message', 'message-received');
@@ -125,10 +130,11 @@ function loadChatMessages() {
 
             messagesElement.appendChild(messageContainer);
         });
-        
+    }
     });
-    
+
     scrollToBottom();
+}
     socket.addEventListener('error', function (event) {
         console.error('WebSocket error: ', event);
         document.getElementById("error").textContent="Ошибка соединения с сервером";
@@ -141,12 +147,10 @@ function loadChatMessages() {
 
     
 function sendMessage() {
+    console.log("Check;;;");
     const input = document.getElementById('messageInput');
     const message = input.value.trim();
     if (message !== '') {
-
-        var socket = new WebSocket('ws://217.197.240.93:8080');
-        socket.addEventListener('open', function (event) {
         console.log('Connected to WS Server');
         
         var jsonData = {
@@ -172,13 +176,6 @@ function sendMessage() {
         document.getElementById('messageInput').value = '';
         console.log("hui piska")
         scrollToBottom();
-    });
-
-        socket.addEventListener('error', function (event) {
-            console.error('WebSocket error: ', event);
-            document.getElementById("error").textContent="Ошибка соединения с сервером";
-
-        });
         const chat = CURRCHID
         
 
@@ -209,9 +206,6 @@ document.getElementById('chatinput').addEventListener('keydown', function(event)
 document.getElementById('sendButtonChat').addEventListener('click', sendReqNewChat);
 
 function sendReqNewChat(){
-    var socket = new WebSocket('ws://217.197.240.93:8080');
-        socket.addEventListener('open', function (event) {
-            console.log('Connected to WS Server');
             
             var jsonData = {
                 type: 3,
@@ -233,7 +227,6 @@ function sendReqNewChat(){
                 document.getElementById("error").textContent="Ошибка соединения с сервером";
             });
             window.location.assign('main.html')
-        });
         
         event.preventDefault();
         
